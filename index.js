eb2a03b90572dfd1e17a309beeb54651c880fc0b
amit sharma <amitbsharma.03@gmail.com>
2024-03-09 15:47:19 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+required('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Helloo World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
